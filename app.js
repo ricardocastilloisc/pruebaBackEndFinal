91cf97b659893321b683bd0988dfb1467bcb32c2
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2023-02-08 10:29:06 -0500

    test2 deploy

--- app.js
+++ app.js
@@ -21,10 +21,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 app.use('/', dna);
-const port =  8010;
-
-app.listen(port, () => {
-  console.log("api rest", port);
-});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
 
 module.exports = app;
